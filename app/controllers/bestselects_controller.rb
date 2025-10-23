9b3915e6bd16e16d1c9e50b178afb1147c027978
kainakamura <yamadeko2761@gmail.com>
2025-10-23 21:00:09 +0900

    画面遷移ロジック

--- app/controllers/bestselects_controller.rb
+++ app/controllers/bestselects_controller.rb
@@ -2,4 +2,8 @@ class BestselectsController < ApplicationController
   def index
     @bestselect = Bestselect.includes(:answers).first
   end
+
+  def show
+    @bestselect = Bestselect.find(params[:id])
+  end
 end
