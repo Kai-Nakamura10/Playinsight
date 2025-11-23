c314c51de77c6309231c1b4bdcd53baf1fa94615
kainakamura <yamadeko2761@gmail.com>
2025-11-23 16:00:18 +0900

    idの代わりにuuidを使用

--- db/migrate/20250930091148_create_videos.rb
+++ db/migrate/20250930091148_create_videos.rb
@@ -1,6 +1,6 @@
 class CreateVideos < ActiveRecord::Migration[8.0]
   def change
-    create_table :videos do |t|
+    create_table :videos, id: :uuid do |t|
       t.references :user, null: false, foreign_key: true
       t.string  :title, null: false
       t.text    :description
