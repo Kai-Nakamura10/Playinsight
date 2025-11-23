9c9daab12c82f9c7ac79d32f5f0a296bb1c81620
kainakamura <yamadeko2761@gmail.com>
2025-11-23 18:22:01 +0900

    利用規約

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :how_to ]
+  skip_before_action :authenticate_user!, only: [ :how_to, :terms ]
   def how_to;end
+  def terms;end
 end
