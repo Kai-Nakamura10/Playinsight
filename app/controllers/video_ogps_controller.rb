134c19b4c77132d61dad8672a2c82270d4560259
kainakamura <yamadeko2761@gmail.com>
2025-12-08 14:02:45 +0900

    OGP10

--- app/controllers/video_ogps_controller.rb
+++ app/controllers/video_ogps_controller.rb
@@ -2,7 +2,7 @@ class VideoOgpsController < ApplicationController
   skip_before_action :authenticate_user!, only: :show
 
   def show
-    @video = Video.find(params[:id])
-    head :not_found if @video.visibility_private?
+    @video = Video.find_by(id: params[:id])
+    return head :not_found if @video.nil? || @video.visibility_private?
   end
 end
